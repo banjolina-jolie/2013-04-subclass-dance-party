7fc353e86d7677fbb0c6498623b7ad98a935a58f
Hack Reactor Students <github@hackreactor.com>
2013-05-04 12:32:13 -0700

    Added comment

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,7 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  //equivalent to Dancer.prototype.constructor
   Dancer.apply(this, arguments);
 };
 
